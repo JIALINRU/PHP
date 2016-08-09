86613c5f7e9c6c98740f022fa65eea6073685f3b
Try my best <293456384@qq.com>
2016-08-09 15:33:11 +0800

    user

--- ci1/application/controllers/user.php
+++ ci1/application/controllers/user.php
@@ -11,5 +11,13 @@
 		public function reg(){
 			$this->load->view('reg.php');
 		}
+		public function do_reg(){
+			//echo 3333;
+			$name=$this->input->post('username');
+			$pass=$this->input->post('pass');
+			$this->load->model('user_model');
+			$this->uesr_model->insert($name,$pass);
+			//echo $name."||".$pass;
+		}
 	}
 ?>
